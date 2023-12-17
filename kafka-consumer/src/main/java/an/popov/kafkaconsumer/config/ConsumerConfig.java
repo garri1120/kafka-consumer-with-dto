b8102b2eaed40aa03bfe85a554df2aff97fad337
Андрей Попов <123795701+garri1120@users.noreply.github.com>
2023-12-17 22:55:36 +0300

    add poison-pill

--- kafka-consumer/src/main/java/an/popov/kafkaconsumer/config/ConsumerConfig.java
+++ kafka-consumer/src/main/java/an/popov/kafkaconsumer/config/ConsumerConfig.java
@@ -1,7 +1,6 @@
 package an.popov.kafkaconsumer.config;
 
 import an.popov.PersonDtoForKafka.dto.PersonDto;
-import com.google.gson.Gson;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +9,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -24,19 +24,26 @@ public class ConsumerConfig {
 
   @Value(value = "${spring.kafka.consumer.group-id}")
   private String groupId;
+  private static final String DLT_TOPIC_SUFFIX = ".dlt";
 
-  @Bean
-  public ConsumerFactory<String, PersonDto> consumerFactory() {
-    Map<String, Object> config = new HashMap<>();
-    config.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-        bootstrapAddress);
-    config.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-        StringDeserializer.class);
-    config.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-            JsonDeserializer.class);
-    config.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, groupId);
-    return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(PersonDto.class));
-  }
+@Bean
+public ConsumerFactory<String, PersonDto> consumerFactory() {
+  Map<String, Object> config = new HashMap<>();
+  config.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+          bootstrapAddress);
+
+  config.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+          ErrorHandlingDeserializer.class);
+  config.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+          ErrorHandlingDeserializer.class);
+
+  config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+  config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,JsonDeserializer.class);
+  config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "an.popov.PersonDtoForKafka.dto.PersonDto");
+
+  config.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, groupId);
+  return new DefaultKafkaConsumerFactory<>(config);
+}
 
   @Bean
   public ConcurrentKafkaListenerContainerFactory<String, PersonDto> kafkaListenerContainerFactory() {
@@ -44,10 +51,5 @@ public class ConsumerConfig {
     concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
     concurrentKafkaListenerContainerFactory.setMissingTopicsFatal(false);
     return concurrentKafkaListenerContainerFactory;
-  }
-
-  @Bean
-  public Gson gsonJsonConverter() {
-    return new Gson();
   }
 }
